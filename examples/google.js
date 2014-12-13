2a8ef6f2173b24d893f2cdbd05b39b4a891cab3a
Chris McIntosh <cmcintosh@wembassy.com>
2014-12-13 10:34:00 +0800

    Setting up stub functions.

--- examples/google.js
+++ examples/google.js
@@ -4,21 +4,9 @@ function init() {
     map = new OpenLayers.Map('map', {
         projection: 'EPSG:3857',
         layers: [
-            new OpenLayers.Layer.Google(
-                "Google Physical",
-                {type: google.maps.MapTypeId.TERRAIN}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Streets", // the default
-                {numZoomLevels: 20}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Hybrid",
-                {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
-            ),
-            new OpenLayers.Layer.Google(
-                "Google Satellite",
-                {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
+            new OpenLayers.Layer.Here(
+                "Normal Day Map",
+                {type: 'normal.day'}
             )
         ],
         center: new OpenLayers.LonLat(10.2, 48.9)
@@ -28,7 +16,7 @@ function init() {
         zoom: 5
     });
     map.addControl(new OpenLayers.Control.LayerSwitcher());
-    
+
     // add behavior to html
     var animate = document.getElementById("animate");
     animate.onclick = function() {
